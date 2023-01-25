461262205bbeba1823d935e4cee949bfb8e74503
512187 <512187@vistacollege.nl>
2023-01-25 12:45:24 +0100

    Mischien een beetje een spel

---
+++ Wieishet/index.php
@@ -0,0 +1,14 @@
+   <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    Online
+    "Who is it?"
+    <div id="r1">Online</div>
+    <div id="r2">"Who is it?"</div><br><br> 
+
+    <div id="r3"><a href="play.php"> Play!</a></div><br><br>
+
+    <div id="r4"><a href="rules.php">Regels</a></div>
+
+</body>
+</html>
---
+++ Wieishet/play.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regels</title>
+    <link rel="stylesheet" href="play.css">
+</head>
+<body>
+<!-- Home Button -->
+<div class="home">
+   <h1><a href="index.php">Home</a><h1>
+    </div>
+
+    <!-- Begin Boxes -->
+
+
+<center>
+<!-- spongebob -->
+<div class="box1">
+    <br>
+    <img src="./pics./SpongeBob_SquarePants_character.svg.png">
+</div>
+
+<br>
+<!-- patrick -->
+<div class="box2">
+    <br>
+    <img src="./pics./16f154ab0ac5bce014f79d649605eb9c.png">
+</div>
+
+<br>
+<!-- gary -->
+<div class="box3">
+    <br>
+    <img src="./pics./Gary_looking_up_stock_art.webp">
+</div>
+
+
+<!-- mrKrabs -->
+<div class="box4">
+    <br>
+    <img src="./pics./Krabs_artwork.webp">
+</div>
+
+<!-- pearl -->
+<br>
+<div class="box5">
+    <br>
+    <img src="./pics./Pearl_Krabs.webp">
+</div>
+
+<!-- sandy -->
+<br>
+<div class="box6">
+    <br>
+    <img src="./pics./Sandy_Cheeks.svg.png">
+</div>
+
+<!-- Plankton -->
+<br>
+<div class="box7">
+    <br>
+    <img src="./pics./2001129-plankton.png">
+</div>
+
+<!-- mrsPuff -->
+<br>
+<div class="box8">
+    <br>
+    <img src="./pics./Mrs._Puff.svg.png">
+</div>
+
+<!-- fatFish -->
+<br>
+<div class="box9">
+    <br>
+    <img src="./pics./spongebob-squarepants-characters-17.webp">
+</div>
+</center>
+
+
+
+
+
+
+
+</body>
+</html>
